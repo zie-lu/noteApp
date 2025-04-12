4c715066bdaf74e05e61e6eb2764b501b052ec8c
Piotr Zieliński <piotrzet20002@wp.pl>
2025-04-12 15:10:32 +0200

    Update notes.ts

--- backend/src/controllers/notes.ts
+++ backend/src/controllers/notes.ts
@@ -5,7 +5,6 @@ import mongoose from "mongoose";
 
 export const getNotes: RequestHandler = async (req, res, next) => {
     try {
-        //throw Error("Error!")
         const notes = await NoteModel.find().exec();
         res.status(200).json(notes);
     } catch (error) {
